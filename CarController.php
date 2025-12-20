aba84481e1bcb0f6272e2c7a044f8ea0d9b98ce5
ammarabbas603-ai <ammarabbas603@gmail.com>
2025-12-20 18:49:30 +0300

    Add files via upload

---
+++ CarController.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class CarController extends Controller {
+    public function index(){ return view('cars.index'); }
+    public function create(){ return view('cars.create'); }
+}
